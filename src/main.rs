2c8afa3a71f227d57a86bcc428d13a9267c6cc6a
ramin <ramiinnss@gmail.com>
2024-06-28 11:55:57 -0400

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,47 @@ fn read_lines(filename: &str) -> Vec<String> {
     result
 }
 
-fn find_calibration_value(text: &str) -> i64 {
+fn find_calibration_value_str_number(text: &str) -> i64 {
+    let re = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").unwrap();
+    let mut digit_strings: Vec<String> = re
+        .find_iter(text)
+        .map(|mat| mat.as_str().to_string())
+        .collect();
+
+    println!("{:?}", digit_strings);
+    for elem in digit_strings.iter_mut() {
+        match &elem[..] {
+            "one" => *elem = String::from("1"),
+            "two" => *elem = String::from("2"),
+            "three" => *elem = String::from("3"),
+            "four" => *elem = String::from("4"),
+            "five" => *elem = String::from("5"),
+            "six" => *elem = String::from("6"),
+            "seven" => *elem = String::from("7"),
+            "eight" => *elem = String::from("8"),
+            "nine" => *elem = String::from("9"),
+            _ => (),
+        }
+    }
+
+    println!("{:?}", digit_strings);
+    println!("================================");
+    let mut result: String = "".to_string();
+    if let (Some(first), Some(last)) = (digit_strings.first(), digit_strings.last()) {
+        result = format!("{}{}", first, last);
+    } else {
+        println!("No digits found in the text");
+    }
+
+    let mut result_int: i64 = 0;
+    match result.parse::<i64>() {
+        Ok(result) => result_int = result,
+        Err(e) => println!("Failed to convert to i64: {}", e),
+    }
+    result_int
+}
+
+fn _find_calibration_value(text: &str) -> i64 {
     let re = Regex::new(r"\d").unwrap();
     let digit_strings: Vec<String> = re
         .find_iter(text)
@@ -37,7 +77,7 @@ fn main() {
     let res = read_lines("./input.txt");
     let mut sum: i64 = 0;
     for txt in &res {
-        sum += find_calibration_value(txt);
+        sum += find_calibration_value_str_number(txt);
     }
     println!("Sum: {:?}", sum);
 }
